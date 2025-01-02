f1f46080d0bf6bf063034a64bb446de2706b2090
jamay byrone <jamie@jamiebyrne.com>
2025-01-02 13:58:31 +0000

    tester

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -29,7 +29,7 @@ loadEnv(process.env.NODE_ENV, process.cwd());
 const medusaConfig = {
   projectConfig: {
     databaseUrl: DATABASE_URL,
-    databaseLogging: false,
+    databaseLogging: true,
     redisUrl: REDIS_URL,
     workerMode: WORKER_MODE,
     http: {
